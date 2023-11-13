c10c6f292f5575c3ec3fcb1bc4e3102b5c3a71b0
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-13 19:55:36 +0100

    add home page

--- src/components/RouterComp.tsx
+++ src/components/RouterComp.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import NavBar from './pages/NavBar'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <NavBar />
+    element: <NavBar />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+    ]
   }
 ])
 
