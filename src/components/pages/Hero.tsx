8a2104b620089be938e9bef80440343864541285
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-14 17:07:46 +0100

    handle next slide after 5s

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -22,11 +22,18 @@ function Hero() {
   const [loading, setLoading] = useState<boolean>(true);
   const [index, setIndex] = useState<number>(0);
 
+  const handleNext = () => {
+    setIndex(index === limit - 1 ? 0 : index + 1);
+  }
+
+  const handlePrev = () => {
+    setIndex(index === 0 ? limit - 1 : index - 1);
+  }
+
   React.useEffect(() => {
     const fetchAnime = () => {
       axios.get(`https://api.jikan.moe/v4/top/anime?filter=airing&limit=${limit}`)
         .then((response) => {
-          console.log(response.data.data[0]);
           setAnimeData(response.data.data);
           setLoading(false);
         })
@@ -41,13 +48,14 @@ function Hero() {
     fetchAnime();
   }, []);
 
-  const handleNext = () => {
-    setIndex(index === limit - 1 ? 0 : index + 1);
-  }
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      handleNext();
+    }, 5000);
 
-  const handlePrev = () => {
-    setIndex(index === 0 ? limit - 1 : index - 1);
-  }
+    return () => clearInterval(interval);
+
+  }, [index])
 
   if (loading) {
     return "Loading ...";
@@ -55,20 +63,23 @@ function Hero() {
 
   return (
     <div className='relative w-full h-[60vh] bg-zinc-950 overflow-x-hidden'>
-      <div className='z-[10] absolute top-0 bottom-0 left-0 w-[3000px] bg-gradient-to-r from-black'></div>
-      <div className='z-[10] absolute bottom-0 right-0 left-0 w-full h-[500px] bg-gradient-to-t from-black/90'></div>
+      <div className='z-[10] absolute top-0 bottom-0 left-0 w-[2000px] bg-gradient-to-r from-zinc-950'></div>
+      <div className='z-[10] absolute bottom-0 right-0 left-0 w-full h-[500px] bg-gradient-to-t from-zinc-950'></div>
       {animeData && animeData.map((anime, key) =>
-        <div className={`absolute inset-0 transition-opacity bg-no-repeat bg-cover ${key === index ? 'opacity-100' : 'opacity-0'}`} style={{ backgroundImage: `url('${anime.trailer.images.maximum_image_url}')` }}>
+        <div className={`absolute inset-0 transition-opacity bg-no-repeat bg-cover ${key === index ? 'opacity-100' : 'opacity-0'}`} key={key} style={{ backgroundImage: `url('${anime.trailer.images.maximum_image_url}')` }}>
           <div className='absolute top-1/2 -translate-y-1/2 left-0 z-[10] px-[65px] max-w-3xl'>
-            <div className='text-[55px] font-medium tracking-tighter leading-[60px]'>{anime.title}</div>
-            <div className='mt-[15px]'>{anime.synopsis.split("").splice(0, 300).join("") + "..."}</div>
+            <div className='text-[55px] text-zinc-50 font-medium tracking-tighter leading-[60px]'>{anime.title}</div>
+            {anime.genres.map((genre, key) =>
+              <span className='mt-[15px] text-[13px] text-zinc-400 inline-block mr-2' key={key}>{genre.name}</span>
+            )}
+            <div className='mt-[15px] text-[14px] text-zinc-200 leading-6'>{anime.synopsis.split("").splice(0, 300).join("") + "..."}</div>
           </div>
         </div>
       )}
-      <div className='z-[15] py-14 px-5 absolute top-1/2 -translate-y-1/2 right-0 flex items-center justify-center cursor-pointer text-[36px]' onClick={handleNext}>
+      <div className='z-[15] py-14 px-2 absolute top-1/2 -translate-y-1/2 right-0 flex items-center justify-center cursor-pointer text-[36px]' onClick={handleNext}>
         <IoIosArrowForward />
       </div>
-      <div className='z-[15] py-14 px-5 absolute top-1/2 -translate-y-1/2 left-0 flex items-center justify-center cursor-pointer text-[36px]' onClick={handlePrev}>
+      <div className='z-[15] py-14 px-2 absolute top-1/2 -translate-y-1/2 left-0 flex items-center justify-center cursor-pointer text-[36px]' onClick={handlePrev}>
         <IoIosArrowBack />
       </div>
     </div>
