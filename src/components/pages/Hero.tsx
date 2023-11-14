e25661f1e10e22c6c096c320aa8a643d4708a86a
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-14 15:07:52 +0100

    add slider animation

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -12,13 +12,14 @@ interface AnimeData {
 }
 
 function Hero() {
+  const limit: number = 6
   const [animeData, setAnimeData] = React.useState<AnimeData[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [index, setIndex] = useState<number>(0);
 
   React.useEffect(() => {
     const fetchAnime = () => {
-      axios.get('https://api.jikan.moe/v4/seasons/now?filter=tv&limit=6')
+      axios.get(`https://api.jikan.moe/v4/seasons/now?filter=tv&limit=${limit}`)
         .then((response) => {
           console.log(response.data.data[0]);
           setAnimeData(response.data.data);
@@ -28,12 +29,20 @@ function Hero() {
           if (error.message === "Network Error") {
             console.log("Network Error")
           }
-          console.log(error)
+          console.log(error.message)
           setLoading(false);
         });
     }
     fetchAnime();
   }, []);
+
+  const handleNext = () => {
+    setIndex(index === limit - 1 ? 0 : index + 1);
+  }
+
+  const handlePrev = () => {
+    setIndex(index === 0 ? limit - 1 : index - 1);
+  }
 
   if (loading) {
     return "Loading ...";
@@ -41,15 +50,16 @@ function Hero() {
 
   return (
     <div className='relative w-full h-[calc(75vh)] bg-slate-950'>
-      {animeData &&
-        <div className='absolute inset-0'>
-          <img src={animeData[0]?.trailer.images.maximum_image_url} className='w-full h-full object-cover pointer-events-none' alt="" />
+      {animeData && animeData.map((anime, key) =>
+        <div className={`absolute inset-0 transition-opacity duration-150 ${key === index ? 'opacity-100' : 'opacity-0'}`}>
+          <img src={anime.trailer.images.maximum_image_url} className={`w-full h-full object-center pointer-events-none`} alt="Anime trailer" />
         </div>
-      }
-      <div className='py-14 px-5 absolute top-1/2 -translate-y-1/2 right-0 flex items-center justify-center cursor-pointer text-[36px]'>
+      )}
+
+      <div className='py-14 px-5 absolute top-1/2 -translate-y-1/2 right-0 flex items-center justify-center cursor-pointer text-[36px]' onClick={handleNext}>
         <IoIosArrowForward />
       </div>
-      <div className='py-14 px-5 absolute top-1/2 -translate-y-1/2 left-0 flex items-center justify-center cursor-pointer text-[36px]'>
+      <div className='py-14 px-5 absolute top-1/2 -translate-y-1/2 left-0 flex items-center justify-center cursor-pointer text-[36px]' onClick={handlePrev}>
         <IoIosArrowBack />
       </div>
     </div>
