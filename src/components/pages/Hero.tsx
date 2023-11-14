1d9bb25610de36005ca4908153b8ae4c5ac3f1a1
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-14 14:52:49 +0100

    add arrows left and right for the slide animation

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -1,8 +1,18 @@
 import axios from 'axios';
 import React from 'react'
+import { IoIosArrowForward } from "@react-icons/all-files/io/IoIosArrowForward";
+import { IoIosArrowBack } from "@react-icons/all-files/io/IoIosArrowBack";
+
+interface AnimeData {
+  trailer: {
+    images: {
+      maximum_image_url: string
+    }
+  }
+}
 
 function Hero() {
-  const [animeData, setAnimeData] = React.useState([]);
+  const [animeData, setAnimeData] = React.useState<AnimeData[]>([]);
 
   React.useEffect(() => {
     const fetchAnime = () => {
@@ -22,14 +32,18 @@ function Hero() {
   }, []);
 
   return (
-    <div className='relative w-full h-[calc(100vh-60px)] bg-slate-950'>
+    <div className='relative w-full h-[calc(75vh)] bg-slate-950'>
       {animeData &&
         <div className='absolute inset-0'>
-          <img src={animeData[0]?.trailer.images.maximum_image_url} className='w-full h-full object-center pointer-events-none' alt="" />
+          <img src={animeData[0]?.trailer.images.maximum_image_url} className='w-full h-full object-cover pointer-events-none' alt="" />
         </div>
       }
-      <div className='py-14 px-5 bg-white/50 absolute top-1/2 -translate-y-1/2 right-0 flex items-center justify-center cursor-pointer'>Next</div>
-      <div className='py-14 px-5 bg-white/50 absolute top-1/2 -translate-y-1/2 left-0 flex items-center justify-center cursor-pointer'>Prev</div>
+      <div className='py-14 px-5 absolute top-1/2 -translate-y-1/2 right-0 flex items-center justify-center cursor-pointer text-[36px]'>
+        <IoIosArrowForward />
+      </div>
+      <div className='py-14 px-5 absolute top-1/2 -translate-y-1/2 left-0 flex items-center justify-center cursor-pointer text-[36px]'>
+        <IoIosArrowBack />
+      </div>
     </div>
   )
 }
