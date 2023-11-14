5fb670966ba650c38bc021b2ab0e71aa1753d21c
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-14 12:36:26 +0100

    make the nav responsive

--- src/components/pages/NavBar.tsx
+++ src/components/pages/NavBar.tsx
@@ -55,12 +55,17 @@ const dropDownVariants = {
     opacity: 1,
     y: 0,
     transition: {
+      duration: 0.4,
       staggerChildren: 0.01
     }
   },
   exit: {
     opacity: 0,
     y: 10,
+    transition: {
+      duration: 0.4,
+      staggerChildren: 0.01
+    }
   }
 }
 
@@ -74,7 +79,7 @@ const NavElement = ({ children, handleClick, isClicked }: NavElementType): JSX.E
 
 const InputElement = (): JSX.Element => {
   return (
-    <input type="text" className='w-[420px] h-[38px] bg-transparent border border-zinc-600 outline-none rounded-sm px-9 text-sm' />
+    <input type="text" className='w-[250] h-[38px] bg-transparent border border-zinc-600 outline-none rounded-sm px-9 text-sm' />
   )
 }
 
@@ -95,54 +100,55 @@ function NavBar() {
     <div>
       <div className='sticky top-0 w-full h-[60px] bg-zinc-800 px-[65px] flex'>
 
-        <a href='' className='w-[170px] px-[18px] h-full flex justify-center items-center'>
-          <img src={shurikenLogo} alt="Shuriken Logo" className='w-full' />
-        </a>
-
-        <nav className='w-full flex justify-between'>
-          <ul className='h-full flex'>
-            <NavElement handleClick={handleClick} isClicked={isClicked}>
-              <span className='group-hover:text-white'>Browse</span>
-              <div className='pl-1 text-white'>
-                <RiArrowDropDownLine className="text-[24px]" />
-              </div>
-              <AnimatePresence>
-                {isClicked &&
-                  <motion.div
-                    variants={dropDownVariants}
-                    initial="hidden"
-                    animate="visible"
-                    exit="exit"
-                    className='absolute left-0 top-full w-[862px] py-[10px] text-[14px] text-zinc-50 bg-zinc-950 rounded-b-md flex'
-                  >
-                    <div className='w-[220px] flex-shrink-0 border-r-2 border-r-zinc-800'>
-                      <div className='py-[12px] px-[16px] text-[13px] text-zinc-400 cursor-pointer uppercase'>Looking for?</div>
-                      {search.map(({ search }, key) =>
-                        <motion.div variants={dropDownVariants} className='py-[12px] px-[16px] cursor-pointer hover:bg-zinc-800' key={key} onClick={() => console.log(key)}>{search}</motion.div>
-                        // <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.05 * key }} className='py-[12px] px-[16px] cursor-pointer hover:bg-zinc-800' key={key} onClick={() => console.log(key)}>{search}</motion.div>
-                      )}
-                    </div>
-                    <div className='w-full'>
-                      <div className='py-[12px] px-[16px] text-[13px] text-zinc-400 cursor-pointer uppercase'>Genre</div>
-                      <div className='grid grid-cols-3'>
-                        {genres.map(({ genre }, key) =>
-                          <motion.div variants={dropDownVariants} className='py-[12px] px-[16px] cursor-pointer hover:bg-zinc-800' key={key}>{genre}</motion.div>
-                          // <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.03 * key }} className='py-[12px] px-[16px] cursor-pointer hover:bg-zinc-800' key={key}>{genre}</motion.div>
+        <nav className='w-full flex justify-center md:justify-between'>
+          <div className='flex'>
+            <a href='' className='h-full flex items-center'>
+              <img src={shurikenLogo} alt="Shuriken Logo" className='w-[190px] px-[18px]' />
+            </a>
+            <ul className='h-full hidden lg:flex'>
+              <NavElement handleClick={handleClick} isClicked={isClicked}>
+                <span className='group-hover:text-white'>Browse</span>
+                <div className='pl-1 text-white'>
+                  <RiArrowDropDownLine className="text-[24px]" />
+                </div>
+                <AnimatePresence>
+                  {isClicked &&
+                    <motion.div
+                      variants={dropDownVariants}
+                      initial="hidden"
+                      animate="visible"
+                      exit="exit"
+                      className='absolute left-0 top-full w-[862px] py-[10px] text-[14px] text-zinc-50 bg-zinc-950 rounded-b-md flex'
+                    >
+                      <div className='w-[220px] flex-shrink-0 border-r-2 border-r-zinc-800'>
+                        <div className='py-[12px] px-[16px] text-[13px] text-zinc-400 cursor-pointer uppercase'>Looking for?</div>
+                        {search.map(({ search }, key) =>
+                          <motion.div variants={dropDownVariants} className='py-[12px] px-[16px] cursor-pointer hover:bg-zinc-800' key={key}>{search}</motion.div>
+                          // <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.05 * key }} className='py-[12px] px-[16px] cursor-pointer hover:bg-zinc-800' key={key} onClick={() => console.log(key)}>{search}</motion.div>
                         )}
                       </div>
-                    </div>
-                  </motion.div>
-                }
-              </AnimatePresence>
-            </NavElement>
-            <NavElement>
-              <span className='group-hover:text-white'>Manga</span>
-            </NavElement>
-            <NavElement>
-              <span className='group-hover:text-white'>News</span>
-            </NavElement>
-          </ul>
-          <div className='flex h-full'>
+                      <div className='w-full'>
+                        <div className='py-[12px] px-[16px] text-[13px] text-zinc-400 cursor-pointer uppercase'>Genre</div>
+                        <div className='grid grid-cols-3'>
+                          {genres.map(({ genre }, key) =>
+                            <motion.div variants={dropDownVariants} className='py-[12px] px-[16px] cursor-pointer hover:bg-zinc-800' key={key}>{genre}</motion.div>
+                            // <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.03 * key }} className='py-[12px] px-[16px] cursor-pointer hover:bg-zinc-800' key={key}>{genre}</motion.div>
+                          )}
+                        </div>
+                      </div>
+                    </motion.div>
+                  }
+                </AnimatePresence>
+              </NavElement>
+              <NavElement>
+                <span className='group-hover:text-white'>Manga</span>
+              </NavElement>
+              <NavElement>
+                <span className='group-hover:text-white'>News</span>
+              </NavElement>
+            </ul>
+          </div>
+          <div className='hidden md:flex h-full'>
             <div className='relative px-[18px] flex items-center'>
               <InputElement />
               <IoSearch className="text-[18px] absolute top-1/2 -translate-y-1/2 left-7 cursor-pointer" />
