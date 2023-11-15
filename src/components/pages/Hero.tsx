c65852adac12c88ea256bc2ccab89b622ca74c3d
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-15 08:48:34 +0100

    add Types to the comp

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -25,6 +25,13 @@ interface AnimeData {
   duration: string
 }
 
+interface ImageTrailer extends Children {
+  className: string,
+  trailer: {
+    images: { maximum_image_url: string }
+  }
+}
+
 const HeroContainer = ({ children, }: Children) => {
   return (
     <div className='relative w-full h-[600px] overflow-x-hidden'>
@@ -33,9 +40,9 @@ const HeroContainer = ({ children, }: Children) => {
   )
 }
 
-const ImageTrailer = ({ children, className, anime }) => {
+const ImageTrailer = ({ children, className, trailer }: (ImageTrailer)) => {
   return (
-    <div className={`absolute inset-0 bg-no-repeat bg-cover bg-top ${className}`} style={{ backgroundImage: `url('${anime.trailer.images.maximum_image_url}')` }}>
+    <div className={`absolute inset-0 bg-no-repeat bg-cover bg-top ${className}`} style={{ backgroundImage: `url('${trailer.images.maximum_image_url}')` }}>
       {children}
     </div>
   )
@@ -95,19 +102,18 @@ function Hero() {
       <div className='z-[10] absolute top-0 bottom-0 left-0 w-[2000px] bg-gradient-to-r from-zinc-950'></div>
       <div className='z-[10] absolute bottom-0 right-0 left-0 w-full h-[500px] bg-gradient-to-t from-zinc-950'></div>
       {animeData && animeData.map((anime, key) =>
-        // <div className={`absolute inset-0 bg-no-repeat bg-cover bg-top ${key === index ? 'opacity-100' : 'opacity-0'}`} key={key} style={{ backgroundImage: `url('${anime.trailer.images.maximum_image_url}')` }}>
-        <ImageTrailer className={`${key === index ? 'opacity-100' : 'opacity-0'}`} anime={anime} key={key}>
+        <ImageTrailer className={`${key === index ? 'opacity-100' : 'opacity-0'}`} trailer={anime.trailer} key={`${key}-${anime.title}`}>
           <div className='absolute h-[500px] flex flex-col justify-between top-1/2 -translate-y-1/2 left-0 z-[10] px-[65px] max-w-3xl'>
             <div className='text-[55px] text-zinc-50 font-medium tracking-tighter mt-[70px] leading-[60px]'>
               {anime.title.length >= 20 ? anime.title.split("").splice(0, 20).join("") + "..." : anime.title}
             </div>
 
             <div className='flex-grow  mt-10'>
-              <span className='text-[13px] text-zinc-400 inline-block items-center mr-5' key={key}>
+              <span className='text-[13px] text-zinc-400 inline-block items-center mr-5'>
                 <IoIosPlay className="mr-1 inline" />
                 {anime.type}
               </span>
-              <span className='text-[13px] text-zinc-400 inline-block mr-5' key={key}>
+              <span className='text-[13px] text-zinc-400 inline-block mr-5'>
                 <FiClock className="mr-1 inline" />
                 {anime.duration === "Unknown" ? "Unknown" : anime.duration.split("").splice(0, 2).join("") + " min"}
               </span>
