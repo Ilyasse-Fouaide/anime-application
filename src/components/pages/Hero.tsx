5a7440372c8ece46c72bf98dbd6331669e0f9b22
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-15 09:12:55 +0100

    add Hero Content container

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -11,7 +11,7 @@ interface Children {
 }
 
 interface Trailer {
-  trailer: {
+  trailer?: {
     images: { maximum_image_url: string }
   }
 }
@@ -40,8 +40,51 @@ const HeroContainer = ({ children, }: Children) => {
 
 const ImageTrailer = ({ children, className, trailer }: (ImageTrailer)) => {
   return (
-    <div className={`absolute inset-0 bg-no-repeat bg-cover bg-top ${className}`} style={{ backgroundImage: `url('${trailer.images.maximum_image_url}')` }}>
+    <div className={`absolute inset-0 bg-no-repeat bg-cover bg-top ${className}`} style={{ backgroundImage: `url('${trailer?.images.maximum_image_url}')` }}>
       {children}
+    </div>
+  )
+}
+
+const HeroContent = ({ title, type, duration, genres, synopsis, index, goTo }: AnimeData & { index: number, goTo: (k: number) => void }) => {
+  return (
+    <div className='absolute h-[500px] flex flex-col justify-between top-1/2 -translate-y-1/2 left-0 z-[10] px-[65px] max-w-3xl'>
+      <div className='text-[55px] text-zinc-50 font-medium tracking-tighter mt-[70px] leading-[60px]'>
+        {title.length >= 20 ? title.split("").splice(0, 20).join("") + "..." : title}
+      </div>
+
+      <div className='flex-grow  mt-10'>
+        <span className='text-[13px] text-zinc-400 inline-block items-center mr-5'>
+          <IoIosPlay className="mr-1 inline" />
+          {type}
+        </span>
+        <span className='text-[13px] text-zinc-400 inline-block mr-5'>
+          <FiClock className="mr-1 inline" />
+          {duration === "Unknown" ? "Unknown" : duration.split("").splice(0, 2).join("") + " min"}
+        </span>
+        {genres.map((genre, key) =>
+          <span className='text-[13px] text-zinc-400 inline-block mr-2' key={key}>{genre.name}</span>
+        )}
+        <div className='mt-1 text-[14px] text-zinc-200 leading-6'>{synopsis.split("").splice(0, 300).join("") + "..."}</div>
+      </div>
+
+      <div className='flex'>
+        <div className='bg-[#DD4854] mr-3 text-zinc-950 font-semibold uppercase inline-flex items-center py-[7px] px-[20px] cursor-pointer'>
+          <IoIosPlay className="mr-2 text-[23px]" />
+          Watch episode
+        </div>
+        <div className='py-[7px] px-[6px] bg-transparent border-2 border-[#DD4854] text-[26px] text-[#DD4854] cursor-pointer'>
+          <IoBookmarkOutline />
+        </div>
+      </div>
+
+      <div className='flex items-center mt-[60px]'>
+        {["", "", "", "", "", ""].map((_el, key) =>
+          <div className={`relative mr-3 h-[10px] transition-all duration-300 bg-zinc-500 cursor-pointer rounded-full overflow-hidden ${key === index ? 'w-[60px]' : 'w-[20px]'}`} key={key} onClick={() => goTo(key)}>
+            <div className={`absolute left-0 bottom-0 top-0 bg-[#DD4854] cursor-pointer ${key === index ? 'w-[100%] transition-all duration-[10s]' : 'w-0'}`}></div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
@@ -99,46 +142,10 @@ function Hero() {
     <HeroContainer>
       <div className='z-[10] absolute top-0 bottom-0 left-0 w-[2000px] bg-gradient-to-r from-zinc-950'></div>
       <div className='z-[10] absolute bottom-0 right-0 left-0 w-full h-[500px] bg-gradient-to-t from-zinc-950'></div>
-      {animeData && animeData.map((anime, key) =>
-        <ImageTrailer className={`${key === index ? 'opacity-100' : 'opacity-0'}`} trailer={anime.trailer} key={`${key}-${anime.title}`}>
-          <div className='absolute h-[500px] flex flex-col justify-between top-1/2 -translate-y-1/2 left-0 z-[10] px-[65px] max-w-3xl'>
-            <div className='text-[55px] text-zinc-50 font-medium tracking-tighter mt-[70px] leading-[60px]'>
-              {anime.title.length >= 20 ? anime.title.split("").splice(0, 20).join("") + "..." : anime.title}
-            </div>
+      {animeData && animeData.map(({ title, type, duration, genres, synopsis, trailer }, key) =>
+        <ImageTrailer className={`${key === index ? 'opacity-100' : 'opacity-0'}`} trailer={trailer} key={`${key}-${title}`}>
+          <HeroContent title={title} type={type} duration={duration} genres={genres} synopsis={synopsis} index={index} goTo={goTo} />
 
-            <div className='flex-grow  mt-10'>
-              <span className='text-[13px] text-zinc-400 inline-block items-center mr-5'>
-                <IoIosPlay className="mr-1 inline" />
-                {anime.type}
-              </span>
-              <span className='text-[13px] text-zinc-400 inline-block mr-5'>
-                <FiClock className="mr-1 inline" />
-                {anime.duration === "Unknown" ? "Unknown" : anime.duration.split("").splice(0, 2).join("") + " min"}
-              </span>
-              {anime.genres.map((genre, key) =>
-                <span className='text-[13px] text-zinc-400 inline-block mr-2' key={key}>{genre.name}</span>
-              )}
-              <div className='mt-1 text-[14px] text-zinc-200 leading-6'>{anime.synopsis.split("").splice(0, 300).join("") + "..."}</div>
-            </div>
-
-            <div className='flex'>
-              <div className='bg-[#DD4854] mr-3 text-zinc-950 font-semibold uppercase inline-flex items-center py-[7px] px-[20px] cursor-pointer'>
-                <IoIosPlay className="mr-2 text-[23px]" />
-                Watch episode
-              </div>
-              <div className='py-[7px] px-[6px] bg-transparent border-2 border-[#DD4854] text-[26px] text-[#DD4854] cursor-pointer'>
-                <IoBookmarkOutline />
-              </div>
-            </div>
-
-            <div className='flex items-center mt-[60px]'>
-              {["", "", "", "", "", ""].map((_el, key) =>
-                <div className={`relative mr-3 h-[10px] transition-all duration-300 bg-zinc-500 cursor-pointer rounded-full overflow-hidden ${key === index ? 'w-[60px]' : 'w-[20px]'}`} key={key} onClick={() => goTo(key)}>
-                  <div className={`absolute left-0 bottom-0 top-0 bg-[#DD4854] cursor-pointer ${key === index ? 'w-[100%] transition-all duration-[10s]' : 'w-0'}`}></div>
-                </div>
-              )}
-            </div>
-          </div>
         </ImageTrailer>
       )}
       <div className='z-[15] py-14 px-2 absolute top-1/2 -translate-y-1/2 right-0 flex items-center justify-center cursor-pointer text-[30px]' onClick={handleNext}>
