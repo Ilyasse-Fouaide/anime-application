82f1179a2e16a384689ca2e3ee56c8fea88d1672
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-16 20:28:58 +0100

    add baseUrl to env file

--- src/axios/axiosClient.ts
+++ src/axios/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'https://api.jikan.moe/v4/',
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 axiosClient.interceptors.request.use(
