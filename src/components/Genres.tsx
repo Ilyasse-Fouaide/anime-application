79ddeff6dd15df763b8f2efeaf909f45f09ac6b9
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-19 11:10:58 +0100

    styling displayed data

--- src/components/Genres.tsx
+++ src/components/Genres.tsx
@@ -8,16 +8,20 @@ function Genres() {
   React.useEffect(() => {
     getRequest('genres/anime')
       .then(({ data }) => {
-        console.log(data);
         setGenres(data.data)
       })
   }, []);
 
   return (
     <div className='mt-20 mb-0 lg:mt-0 lg:mb-20'>
-      {genres && genres.map(({ name, count }, key) =>
-        <div>{name}</div>
-      )}
+      <div className='px-[20px] lg:px-[65px] mb-[16px] grid grid-cols-6 gap-6'>
+        {genres && genres.map(({ name, count }, key) =>
+          <div className='bg-red-500 relative w-full aspect-square rounded-lg flex items-end' key={key}>
+            <p className='z-10 relative bg-red-500 p-5 uppercase font-bold text-xl text-zinc-50'>{name}</p>
+            <div className='absolute border-[8px] border-zinc-50 w-[90%] h-[90%] top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2'></div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
