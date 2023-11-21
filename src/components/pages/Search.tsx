25ff0b85020ecf74e3fa80ab9878e2de4a09559c
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-21 09:01:00 +0100

    add debounced value

--- src/components/pages/Search.tsx
+++ src/components/pages/Search.tsx
@@ -2,10 +2,19 @@ import React from 'react'
 import { useDebounce } from "use-debounce";
 
 function Search() {
+  const [inputValue, setInputValue] = React.useState("");
+  const [debouncedValue] = useDebounce(inputValue, 500);
+
+  const handleInputChange = (event: any) => {
+    setInputValue(event.target.value);
+  }
+
   return (
-    <div className='bg-zinc-900 py-[32px] flex justify-center'>
-      <input type="text" placeholder='Search...' className='py-1 w-[900px] text-3xl font-semibold bg-transparent border-b-2 transition-colors border-b-zinc-600 focus:border-b-[var(--red)] outline-none' />
-    </div>
+    <>
+      <div className='bg-zinc-900 py-[32px] flex justify-center'>
+        <input type="text" placeholder='Search...' className='py-1 w-[900px] text-3xl font-semibold bg-transparent border-b-2 transition-colors border-b-zinc-600 focus:border-b-[var(--red)] outline-none' value={inputValue} onChange={handleInputChange} />
+      </div>
+    </>
   )
 }
 
