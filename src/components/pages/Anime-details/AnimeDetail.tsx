0ba683b93deb7d36b8e1105b85b4174335e44e24
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-24 11:35:56 +0100

    adding title detail

--- src/components/pages/Anime-details/AnimeDetail.tsx
+++ src/components/pages/Anime-details/AnimeDetail.tsx
@@ -3,9 +3,10 @@ import { getRequest } from '../../../axios/axiosClient';
 import { useParams } from 'react-router-dom';
 import { AnimeData } from '../../Types/types';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { sliceText } from '../../functions/sliceText';
 
 function AnimeDetail() {
-  const [AnimeDetail, setAnimeDtail] = React.useState<AnimeData | null>(null);
+  const [animeDetail, setAnimeDtail] = React.useState<AnimeData | null>(null);
   const [loading, setLoading] = React.useState(true);
   const { id } = useParams();
 
@@ -30,21 +31,27 @@ function AnimeDetail() {
   }
 
   return (
-    <div className='relative w-full h-[500px] bg-no-repeat bg-cover bg-center overflow-hidden' style={{ backgroundImage: `url('${AnimeDetail?.images.jpg.large_image_url}')` }}>
-      <div className='relative z-10 w-full h-full flex justify-center'>
-        <LazyLoadImage
-          src={AnimeDetail?.images.jpg.large_image_url}
-          placeholderSrc={AnimeDetail?.images.jpg.small_image_url}
-          effect="blur"
-          alt={AnimeDetail?.title}
-          className="h-full pointer-events-none"
-        />
+    <>
+      <div className='relative w-full h-[500px] bg-no-repeat bg-cover bg-center overflow-hidden' style={{ backgroundImage: `url('${animeDetail?.images.jpg.large_image_url}')` }}>
+        <div className='relative z-10 w-full h-full flex justify-center'>
+          <LazyLoadImage
+            src={animeDetail?.images.jpg.large_image_url}
+            placeholderSrc={animeDetail?.images.jpg.small_image_url}
+            effect="blur"
+            alt={animeDetail?.title}
+            className="h-full object-cover pointer-events-none select-none"
+          />
+        </div>
+
+        {/* Effects */}
+        <div className='absolute inset-0 bg-zinc-950/80 backdrop-blur-md'></div>
+        <div className='absolute top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2 w-[500px] aspect-square rounded-full bg-zinc-950 blur-3xl'></div>
       </div>
 
-      {/* Effects */}
-      <div className='absolute inset-0 bg-zinc-950/80 backdrop-blur-md'></div>
-      <div className='absolute top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2 w-[500px] aspect-square rounded-full bg-zinc-950 blur-3xl'></div>
-    </div>
+      <div className='max-w-5xl mx-auto p-6'>
+        <h1 className='max-w-xl text-[40px] text-zinc-50 font-[500] leading-[45px]'>{sliceText(animeDetail?.title!, 50)}</h1>
+      </div>
+    </>
   )
 }
 
