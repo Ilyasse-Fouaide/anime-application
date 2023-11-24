8750b6ad4b6bf0be24579a792ec2aef10dbf8738
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-24 21:34:16 +0100

    adding styling if episode N/A and make show less and more for episodes

--- src/components/pages/Anime-details/AnimeDetail.tsx
+++ src/components/pages/Anime-details/AnimeDetail.tsx
@@ -8,13 +8,15 @@ import { Tooltip } from 'react-tooltip'
 import { IoIosPlay } from '@react-icons/all-files/io/IoIosPlay';
 import { IoBookmarkOutline } from '@react-icons/all-files/io5/IoBookmarkOutline';
 import { IoStar } from '@react-icons/all-files/io5/IoStar';
-
 import 'react-tooltip/dist/react-tooltip.css'
+import cat from "../../../assets/cat.svg";
+
 import { formatNumber } from '../Card/CardInfo';
 
 function Episode() {
   const [animeVideo, setAnimeVideo] = React.useState([]);
   const [loading, setLoading] = React.useState<boolean>(true);
+  const [detail, setDetail] = React.useState(true);
   const { id, title: slug } = useParams();
 
   React.useEffect(() => {
@@ -37,26 +39,55 @@ function Episode() {
     return "loading ...";
   }
 
-  return (
-    <div className='mt-10'>
-      <div className='grid grid-cols-1 min-[580px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 min-[580px]:gap-5 lg:gap-6'>
-        {animeVideo && animeVideo.episodes.map(({ images, title, episode }, key) =>
-          <div className='w-full flex flex-row min-[580px]:flex-col cursor-pointer' key={key}>
-            {images.jpg.image_url ?
-              <img src={images.jpg.image_url} className='flex-shrink-0 w-[50%] min-[580px]:w-full mr-3 aspect-video' alt={title} />
-              :
-              <div className='flex-shrink-0 w-[50%] min-[580px]:w-full mr-3 aspect-video bg-zinc-800 flex items-center justify-center'>
-                <IoIosPlay className="text-zinc-500 w-[45px] h-[45px]" />
-              </div>
-            }
-            <div className='mt-0 min-[580px]:mt-3 font-medium text-white'>
-              <div className='text-xs text-zinc-400'>{slug}</div>
-              <span className='text-[13px] min-[580px]:text-[14px] text-white'>{episode} - {sliceText(title, 22)}</span>
-            </div>
-          </div>
-        )}
+  const handleClick = () => {
+    setDetail(!detail)
+  }
+
+  if (animeVideo.episodes.length === 0) {
+    return (
+      <div className='mt-16 border border-dashed flex flex-col items-center space-y-5 p-5'>
+        <div className='w-[250px] h-[250px]'>
+          <img src={cat} alt="CAT" className='w-full h-full pointer-events-none' />
+        </div>
+        <div className='max-w-md text-center'>
+          <div className='text-sm font-medium'>We couldn't find any available. Check back later or explore other content</div>
+        </div>
       </div>
-    </div>
+    )
+  }
+
+  const twelveEP = animeVideo.episodes.length > 12;
+
+  return (
+    <>
+      <div className='relative mt-16 overflow-y-hidden' style={{ height: detail && twelveEP ? "650px" : "auto" }}>
+        <div className='grid grid-cols-1 min-[580px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 min-[580px]:gap-5 lg:gap-6'>
+          {animeVideo && animeVideo.episodes.map(({ images, title, episode }, key) =>
+            <div className='w-full flex flex-row min-[580px]:flex-col cursor-pointer' key={key}>
+              {images.jpg.image_url ?
+                <img src={images.jpg.image_url} className='flex-shrink-0 w-[50%] min-[580px]:w-full mr-3 aspect-video' alt={title} />
+                :
+                <div className='flex-shrink-0 w-[50%] min-[580px]:w-full mr-3 aspect-video bg-zinc-800 flex items-center justify-center'>
+                  <IoIosPlay className="text-zinc-500 w-[45px] h-[45px]" />
+                </div>
+              }
+              <div className='mt-0 min-[580px]:mt-3 font-medium text-white'>
+                <div className='text-xs text-zinc-400'>{slug}</div>
+                <span className='text-[13px] min-[580px]:text-[14px] text-white'>{episode} - {sliceText(title, 22)}</span>
+              </div>
+            </div>
+          )}
+        </div>
+        {detail && twelveEP && <div className='absolute bottom-0 w-full h-[500px] bg-gradient-to-t from-zinc-950'></div>}
+      </div>
+      <div className='mt-4 w-fit text-xs font-semibold text-zinc-400 hover:text-zinc-100 cursor-pointer transition-colors' onClick={handleClick}>
+        {twelveEP &&
+          <>
+            {detail ? "MORE EPISODES" : "LESS EPISODES"}
+          </>
+        }
+      </div>
+    </>
   )
 }
 
