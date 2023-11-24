665b36212078cfdc1e4b7b1d6fe30fcb9c8f5d87
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-24 12:13:26 +0100

    add some info

--- src/components/pages/Anime-details/AnimeDetail.tsx
+++ src/components/pages/Anime-details/AnimeDetail.tsx
@@ -5,8 +5,39 @@ import { AnimeData } from '../../Types/types';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { sliceText } from '../../functions/sliceText';
 import { Tooltip } from 'react-tooltip'
+import { IoIosPlay } from '@react-icons/all-files/io/IoIosPlay';
+import { IoBookmarkOutline } from '@react-icons/all-files/io5/IoBookmarkOutline';
 
 import 'react-tooltip/dist/react-tooltip.css'
+
+function Decription({ animeDetail }: any) {
+  const [detail, setDetail] = React.useState(true);
+
+  const handleClick = () => {
+    setDetail(!detail)
+  }
+
+  return (
+    <>
+      {animeDetail?.synopsis.length >= 500 ?
+        <>
+          <div className='relative mt-6 overflow-hidden' style={{ height: detail ? "5rem" : "auto" }}>
+            <p className='text-zinc-50 text-sm font-medium'>
+              {animeDetail?.synopsis}
+            </p>
+            {detail && <div className='absolute bottom-0 w-full h-[50px] bg-gradient-to-t from-zinc-950'></div>}
+          </div>
+          <div className='mt-2 w-fit text-xs font-semibold text-zinc-400 hover:text-zinc-100 cursor-pointer transition-colors' onClick={handleClick}>
+            MORE DETAIL
+          </div>
+        </>
+        :
+        <p className='mt-6 text-zinc-50 text-sm font-medium'>{animeDetail?.synopsis}</p>
+      }
+
+    </>
+  )
+}
 
 function AnimeDetail() {
   const [animeDetail, setAnimeDtail] = React.useState<AnimeData | null>(null);
@@ -55,6 +86,21 @@ function AnimeDetail() {
         {animeDetail?.title.length! >= 50 &&
           <Tooltip id="my-tooltip" place='bottom' />
         }
+
+        {/* Buttons */}
+        <div className='flex justify-center lg:justify-start mt-6'>
+          <div className='flex items-center uppercase py-[8px] px-[15px] bg-zinc-950 font-semibold text-[var(--red)] cursor-pointer mr-2 border-2 border-[var(--red)]'>
+            <IoIosPlay className="mr-2 text-[23px]" />
+            watch episode
+          </div>
+          <div className='flex items-center uppercase py-[8px] px-[15px] bg-transparent text-zinc-400 font-semibold hover:text-zinc-100 hover:bg-zinc-800 cursor-pointer transition-colors'>
+            <IoBookmarkOutline className="mr-2 text-[26px]" />
+            Add To Watch list
+          </div>
+        </div>
+
+        <Decription animeDetail={animeDetail} />
+
       </div>
     </>
   )
