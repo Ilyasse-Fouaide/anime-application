1067a04cd186fd67c07cd1c92c14764b9a1e9f40
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-24 10:43:57 +0100

    add some styling to the anime detail

--- src/components/pages/Anime-details/AnimeDetail.tsx
+++ src/components/pages/Anime-details/AnimeDetail.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { getRequest } from '../../../axios/axiosClient';
 import { useParams } from 'react-router-dom';
 import { AnimeData } from '../../Types/types';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 function AnimeDetail() {
   const [AnimeDetail, setAnimeDtail] = React.useState<AnimeData | null>(null);
@@ -25,17 +26,24 @@ function AnimeDetail() {
   }, [])
 
   if (loading) {
-    return "Loading ...";
+    return "";
   }
 
   return (
-    <div className='relative w-full h-[600px] bg-no-repeat bg-cover bg-center' style={{ backgroundImage: `url('${AnimeDetail?.images.jpg.large_image_url}')` }}>
-      <div>
-        {/* content */}
+    <div className='relative w-full h-[500px] bg-no-repeat bg-cover bg-center overflow-hidden' style={{ backgroundImage: `url('${AnimeDetail?.images.jpg.large_image_url}')` }}>
+      <div className='relative z-10 w-full h-full flex justify-center'>
+        <LazyLoadImage
+          src={AnimeDetail?.images.jpg.large_image_url}
+          placeholderSrc={AnimeDetail?.images.jpg.small_image_url}
+          effect="blur"
+          alt={AnimeDetail?.title}
+          className="h-full pointer-events-none"
+        />
       </div>
+
       {/* Effects */}
-      <div className='absolute inset-0 bg-zinc-950/80 backdrop-blur-md backdrop-grayscale'></div>
-      {/* <div className='absolute inset-'></div> */}
+      <div className='absolute inset-0 bg-zinc-950/80 backdrop-blur-md'></div>
+      <div className='absolute top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2 w-[500px] aspect-square rounded-full bg-zinc-950 blur-3xl'></div>
     </div>
   )
 }
