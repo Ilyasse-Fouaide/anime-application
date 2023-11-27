9b181f688eb4feb60d4c8d7adc19db1eec48cd61
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-27 13:11:48 +0100

    Update Recommendations component to use List component

--- src/components/pages/Anime-details/Recommendations.tsx
+++ src/components/pages/Anime-details/Recommendations.tsx
@@ -7,6 +7,26 @@ import SkeletonRecomm from '../../Skeleton/SkeletonRecomm';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { sliceText } from '../../functions/sliceText';
 
+function List({ entry, votes }: any) {
+  return (
+    <div className='relative w-[135px] min-[375px]:w-[160px] sm:w-[125px] md:w-[157px] lg:w-[132px] xl:w-[150px] aspect-[2/3] inline-block'>
+      <div className='w-full aspect-[2/3]'>
+        <LazyLoadImage
+          src={entry.images.webp.large_image_url}
+          placeholderSrc={entry.images.webp.small_image_url}
+          effect="blur"
+          alt={entry.title}
+          className="w-full aspect-[2/3] object-cover pointer-events-none"
+        />
+      </div>
+      <div className='text-xs text-zinc-50 font-medium mt-3'>{sliceText(entry.title, 15)}</div>
+      <div className='text-xs text-zinc-400 mt-3 flex items-center'>
+        <p className='mr-2'>{votes} Votes</p>
+      </div>
+    </div>
+  )
+}
+
 function Recommendations() {
   const [anime, setAnime] = React.useState<any[]>([]);
   const [loading, setLoading] = React.useState<boolean>(true);
@@ -59,23 +79,7 @@ function Recommendations() {
       <div className='relative'>
         <div id='slider' className='space-x-2 sm:space-x-6 md:space-x-8 lg:space-x-10 xl:space-x-4 w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth no-scrollbar'>
           {anime && anime.map(({ entry, votes }, key) => {
-            return (
-              <div className='relative w-[135px] min-[375px]:w-[160px] sm:w-[125px] md:w-[157px] lg:w-[132px] xl:w-[150px] aspect-[2/3] inline-block' key={key}>
-                <div className='w-full aspect-[2/3]'>
-                  <LazyLoadImage
-                    src={entry.images.webp.large_image_url}
-                    placeholderSrc={entry.images.webp.small_image_url}
-                    effect="blur"
-                    alt={entry.title}
-                    className="w-full aspect-[2/3] object-cover pointer-events-none"
-                  />
-                </div>
-                <div className='text-xs text-zinc-50 font-medium mt-3'>{sliceText(entry.title, 15)}</div>
-                <div className='text-xs text-zinc-400 mt-3 flex items-center'>
-                  <p className='mr-2'>{votes} Votes</p>
-                </div>
-              </div>
-            )
+            return <List entry={entry} votes={votes} key={key} />
           })}
         </div>
         <div className='hidden md:block absolute w-[36px] h-[36px] top-1/3 right-0 translate-x-1/2  bg-[var(--red)] p-1 rounded-full cursor-pointer shadow-xl shadow-black/30' onClick={slideRight}>
